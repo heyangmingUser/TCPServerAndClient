91bf6ca4f04f9d8f2ebf7fd915adbb471739430d
HYM <244781212@qq.com>
2020-01-06 09:50:03 +0800

    添加注释

--- TCPClient/Program.cs
+++ TCPClient/Program.cs
@@ -27,23 +27,31 @@ namespace TCPClient
             //客户端指定计算机之间通信的协议,套接字类型规定了可以使用什么样的协议，寻址方案
             Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             //通过Ip连接
-            clientSocket.Connect("192.168.1.186", 2000);
-            //发送的数据
+            clientSocket.Connect("192.168.1.212", 2000);
+            //模拟的数据
             string senddata = DateTime.Now.ToString("yyyyMMddHHmmss") + "-" + new Random((int)DateTime.Now.Ticks).Next().ToString();
+            //发送数据
             clientSocket.Send(Encoding.Default.GetBytes(senddata));
             Console.WriteLine("我告诉服务器:" + senddata);
+            //接收超时时间
             clientSocket.ReceiveTimeout = 3000;
+            //list集合
             List<byte> data = new List<byte>();
+            //创建缓存区,
             byte[] buffer = new byte[1024];
             int length = 0;
             try
             {
+                //接收到的字节长度,来自服务器
                 while ((length = clientSocket.Receive(buffer)) > 0)
                 {
+                    //当字节长度小于总的长度时，将他添加到集合中
                     for (int j = 0; j < length; j++)
                     {
+
                         data.Add(buffer[j]);
                     }
+                    //当长度小于缓存的长度时，跳出循环
                     if (length < buffer.Length)
                     {
                         break;
--- TCPServer/Program.cs
+++ TCPServer/Program.cs
@@ -10,18 +10,23 @@ namespace TCPServer
     {
         static void Main(string[] args)
         {
+            //什么是Socket,是计算机之前进行通信的一种约定或方式
+            //客户端指定计算机之间通信的协议,套接字类型规定了可以使用什么样的协议，寻址方案
             Socket skt = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            skt.Bind(new IPEndPoint(IPAddress.Parse("192.168.1.186"), 2000));//前面的局域网内ip,后面是端口号，随便起，在本机不被占用就行
+            //将套接字绑定IP地址
+            skt.Bind(new IPEndPoint(IPAddress.Parse("192.168.1.212"), 2000));//前面的局域网内ip,后面是端口号，随便起，在本机不被占用就行
+            //套接字监听，参数挂起套接字最大的长度
             skt.Listen(100);
             while (true)//不断轮询，接收客户端发送的信息
             {
                 Socket acceptskt = skt.Accept();//接收客户端信息
-                acceptskt.ReceiveTimeout = 3000;//
+                acceptskt.ReceiveTimeout = 3000;//接收超时时间
                 List<byte> data = new List<byte>();
                 byte[] buffer = new byte[1024];
                 int length = 0;
                 try
                 {
+                    //接收到的消息，来自客户端
                     while ((length = acceptskt.Receive(buffer)) > 0)
                     {
                         for (int i = 0; i < length; i++)
@@ -39,6 +44,7 @@ namespace TCPServer
                 {
                     Console.WriteLine(Encoding.Default.GetString(data.ToArray(), 0, data.Count));
                 }
+                //服务端发送给客户端的信息
                 acceptskt.Send(Encoding.Default.GetBytes("我收到你的信了"));
                 if (acceptskt.Connected)
                 {
